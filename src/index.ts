6271bbf982c4ff64570f2e4f7e86765daa017418
Francisco Lopez Tancredi <flopezt@fi.uba.ar>
2026-05-11 19:52:32 -0300

    feat(auth): add password reset schemas (US-03)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './auth/register';
 export * from './auth/login';
+export * from './auth/password-reset';
 export * from './auth/vehicle';
 export * from './errors/error-codes';
 export * from './errors/problem-details';
